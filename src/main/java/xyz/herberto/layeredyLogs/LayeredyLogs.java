1c096cb74894e273552f130dfe7ccc4401780ae2
herberto <herberto@herberto.xyz>
2026-08-31 17:52:55 -0700

    plugin base; add layeredy logs command & config

--- src/main/java/xyz/herberto/layeredyLogs/LayeredyLogs.java
+++ src/main/java/xyz/herberto/layeredyLogs/LayeredyLogs.java
@@ -1,12 +1,34 @@
 package xyz.herberto.layeredyLogs;
 
+import co.aikar.commands.PaperCommandManager;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.herberto.layeredyLogs.commands.LayeredyLogsCommand;
+
+import java.util.Arrays;
 
 public final class LayeredyLogs extends JavaPlugin {
+    @Getter public static LayeredyLogs instance;
+    @Getter public static String loggingToken;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+
+        PaperCommandManager commandManager = new PaperCommandManager(this);
+        commandManager.enableUnstableAPI("help");
+
+        Arrays.asList(
+                new LayeredyLogsCommand()
+        ).forEach(command -> commandManager.registerCommand(command, true));
+
+        loggingToken = getConfig().getString("layeredy-logs-token", "log_tok_1234567890abcdef");
+
+        if(loggingToken.isEmpty() || loggingToken.equals("log_tok_1234567890abcdef")) {
+            getLogger().warning("The logging token is not set in the config.yml file. Get a token at https://app.layeredy.com/dashboard/logs");
+            getLogger().warning("Your server will NOT send logs until it is configured with a valid token.");
+        }
+
     }
 
     @Override
---
+++ src/main/java/xyz/herberto/layeredyLogs/commands/LayeredyLogsCommand.java
@@ -0,0 +1,30 @@
+package xyz.herberto.layeredyLogs.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.CommandHelp;
+import co.aikar.commands.annotation.*;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.command.CommandSender;
+import xyz.herberto.layeredyLogs.LayeredyLogs;
+
+@CommandAlias("layeredylogs|logs")
+public class LayeredyLogsCommand extends BaseCommand {
+    private final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.legacyAmpersand();
+
+    @HelpCommand
+    @Default
+    public void help(CommandSender sender, CommandHelp help) {
+        help.showHelp();
+    }
+
+    @Subcommand("reload")
+    @CommandPermission("layeredylogs.command.reload")
+    @Description("Reload the Layeredy Logs config")
+    public void reload(CommandSender sender) {
+        LayeredyLogs.getInstance().reloadConfig();
+
+        sender.sendMessage(LEGACY.serialize(Component.text("&aReloaded the Layeredy Logs config!")));
+    }
+
+}
